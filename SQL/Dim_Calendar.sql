9490be19ab0304df269caafc3c47c8d509dbf4b8
mewahanggaurav <162393167+mewahanggaurav@users.noreply.github.com>
2025-08-02 22:12:54 -0400

    Add files via upload

---
+++ SQL/Dim_Calendar.sql
@@ -0,0 +1,15 @@
+-- Cleansed DIM_Date Table --
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date,
+  [EnglishDayNameOfWeek] AS Day,
+  [WeekNumberOfYear] AS Week,
+  [EnglishMonthName] AS Month, 
+  Left([EnglishMonthName], 3) AS MonthShort, 
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year
+FROM 
+ [AdventureWorksDW2022].[dbo].[DimDate]
+ WHERE 
+  CalendarYear >= 2019
